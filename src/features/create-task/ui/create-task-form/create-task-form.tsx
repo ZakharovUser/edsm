b738bc45e82107706aefb3f0f5cf9c22c841537f
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-05-22 11:27:16 +0300

    Add create-task-form

---
+++ src/features/create-task/ui/create-task-form/create-task-form.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from 'react';
+import { Form, Input, Select, FormInstance } from 'antd';
+
+// -----------------------------------------------------------------------------------------------------------------
+
+const importance_options = [
+  { label: 'Обычно', value: 'usually' },
+  { label: 'Важно', value: 'important' },
+];
+
+const importance_cause_options = [
+  { label: 'Аварийная ситуация', value: 'emergency' },
+  { label: 'Ошибка планирования', value: 'planning_error' },
+  { label: 'Позднее доведение лимитов', value: 'late_finished_limits' },
+];
+
+const initial = {
+  importance: 'usually',
+};
+
+// -----------------------------------------------------------------------------------------------------------------
+
+interface Props {
+  onInitForm(form: FormInstance): void;
+}
+
+export function CreateTaskForm({ onInitForm }: Props) {
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    onInitForm(form);
+  }, [form, onInitForm]);
+
+  const importanceValue = Form.useWatch('importance', form);
+
+  const isImportant = importanceValue === 'important';
+
+  return (
+    <Form initialValues={initial} form={form} autoComplete="off" layout="vertical">
+      <Form.Item label="Важность" name="importance" rules={[{ required: true }]}>
+        <Select options={importance_options} dropdownStyle={{ zIndex: 1500 }} />
+      </Form.Item>
+      {isImportant && (
+        <Form.Item
+          label="Причина"
+          name="importance_cause"
+          rules={[{ required: true, message: 'Выберите причину' }]}
+        >
+          <Select options={importance_cause_options} dropdownStyle={{ zIndex: 1500 }} />
+        </Form.Item>
+      )}
+      <Form.Item
+        name="name_short"
+        label="Наименование (короткое)"
+        rules={[{ required: true, message: 'Введите короткое наименование' }]}
+      >
+        <Input maxLength={40} />
+      </Form.Item>
+      <Form.Item
+        name="name_full"
+        label="Наименование (полное)"
+        rules={[{ required: true, message: 'Введите полное наименование' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="source_financing"
+        label="Источник финансирования"
+        rules={[{ required: true, message: 'Выберите источник финансирования' }]}
+      >
+        <Select />
+      </Form.Item>
+    </Form>
+  );
+}
