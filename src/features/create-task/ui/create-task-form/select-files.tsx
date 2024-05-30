10f165ebdd5623ce31535887a44b00f19727adc3
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-05-30 15:05:36 +0300

    Add: text to dragger

--- src/features/create-task/ui/create-task-form/select-files.tsx
+++ src/features/create-task/ui/create-task-form/select-files.tsx
@@ -3,13 +3,15 @@ import { InboxOutlined } from '@ant-design/icons';
 
 const { Dragger } = Upload;
 
-interface Props {}
-
-export function SelectFiles(props: Props) {
+export function SelectFiles() {
   return (
     <Dragger multiple>
       <p className="ant-upload-drag-icon">
         <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">Нажмите или перетащите файл в эту область, чтобы загрузить</p>
+      <p className="ant-upload-hint">
+        Поддержка одиночной или массовой загрузки. Строго запрещено загружать запрещенные файлы.
       </p>
     </Dragger>
   );
