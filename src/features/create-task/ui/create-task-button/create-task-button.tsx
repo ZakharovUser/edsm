afda6fdedd887c32e8c004c2a42bd885a20ba334
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-07-04 17:46:28 +0300

    Move: create task button

--- src/features/create-task/ui/create-task-button/create-task-button.tsx
+++ src/features/create-task/ui/create-task-button/create-task-button.tsx
@@ -4,6 +4,8 @@ import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
 
+// -----------------------------------------------------------------------------------------------------------------
+
 interface Props {
   onClick: VoidFunction;
 }
