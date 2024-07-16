5f227affa48425abc9ec2d2d65f8db84c3475c6c
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-07-16 11:45:56 +0300

    Add: open drawer when active search params

--- src/sections/outbox/outbox.view.tsx
+++ src/sections/outbox/outbox.view.tsx
@@ -1,6 +1,9 @@
 import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { useSettingsContext } from 'components/settings';
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 import Container from '@mui/material/Container';
 
 import { useGetOutbox } from 'sections/outbox/hooks';
@@ -12,13 +15,38 @@ import { convertTaskToRow } from 'entites/outbox/helpers';
 
 export function OutboxView() {
   const { data, isLoading } = useGetOutbox();
+  const [searchParams, setSearchParams] = useSearchParams();
   const settings = useSettingsContext();
 
   const inbox_rows = useMemo(() => data?.map(convertTaskToRow).reverse(), [data]);
 
+  const searchedTask = searchParams.get('task');
+
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
-      <OutboxDataGrid rows={inbox_rows || []} loading={isLoading} />
+      <OutboxDataGrid
+        loading={isLoading}
+        rows={inbox_rows || []}
+        onRowClick={(params) => {
+          setSearchParams(new URLSearchParams({ task: params.id.toString() }));
+        }}
+      />
+      <Drawer
+        open={!!searchedTask}
+        onClose={() => setSearchParams()}
+        anchor="right"
+        hideBackdrop
+        disableScrollLock
+        keepMounted={false}
+        sx={{
+          width: 0,
+        }}
+        PaperProps={{
+          sx: { boxShadow: '-2px 0 20px 0 rgba(145, 158, 171, 0.3) !important' },
+        }}
+      >
+        <Box sx={{ px: 2 }}>Hello, {searchedTask}</Box>
+      </Drawer>
     </Container>
   );
 }
