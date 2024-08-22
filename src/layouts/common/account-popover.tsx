15ce5fe629fb9563163f6dc33a524ceb7686fb14
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-08-22 14:13:53 +0300

    Comment: account options

--- src/layouts/common/account-popover.tsx
+++ src/layouts/common/account-popover.tsx
@@ -1,11 +1,11 @@
+import { _mock } from '_mock';
 import { m } from 'framer-motion';
 import { useAuthContext } from 'auth/hooks';
 import { varHover } from 'components/animate';
-import { useMockedUser } from 'hooks/use-mocked-user';
 import CustomPopover, { usePopover } from 'components/custom-popover';
 
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
+// import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
 import Divider from '@mui/material/Divider';
 import { alpha } from '@mui/material/styles';
@@ -17,29 +17,27 @@ import { useRouter } from 'routes/hooks';
 
 // ----------------------------------------------------------------------
 
-const OPTIONS = [
-  {
-    label: 'Home',
-    linkTo: '/',
-  },
-  {
-    label: 'Profile',
-    linkTo: '/#1',
-  },
-  {
-    label: 'Settings',
-    linkTo: '/#2',
-  },
-];
+// const OPTIONS = [
+//   {
+//     label: 'Home',
+//     linkTo: '/',
+//   },
+//   {
+//     label: 'Profile',
+//     linkTo: '/#1',
+//   },
+//   {
+//     label: 'Settings',
+//     linkTo: '/#2',
+//   },
+// ];
 
 // ----------------------------------------------------------------------
 
 export default function AccountPopover() {
   const router = useRouter();
 
-  const { user } = useMockedUser();
-
-  const { logout } = useAuthContext();
+  const { user, logout } = useAuthContext();
 
   const popover = usePopover();
 
@@ -53,10 +51,12 @@ export default function AccountPopover() {
     }
   };
 
-  const handleClickItem = (path: string) => {
-    popover.onClose();
-    router.push(path);
-  };
+  // const handleClickItem = (path: string) => {
+  //   popover.onClose();
+  //   router.push(path);
+  // };
+
+  const displayName = `${user?.last_name} ${user?.first_name}`;
 
   return (
     <>
@@ -77,22 +77,22 @@ export default function AccountPopover() {
         }}
       >
         <Avatar
-          src={user?.photoURL}
-          alt={user?.displayName}
+          src={_mock.image.avatar(4)}
+          alt={displayName}
           sx={{
             width: 36,
             height: 36,
             border: (theme) => `solid 2px ${theme.palette.background.default}`,
           }}
         >
-          {user?.displayName.charAt(0).toUpperCase()}
+          {displayName.charAt(0).toUpperCase()}
         </Avatar>
       </IconButton>
 
       <CustomPopover open={popover.open} onClose={popover.onClose} sx={{ width: 200, p: 0 }}>
         <Box sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="subtitle2" noWrap>
-            {user?.displayName}
+            {displayName}
           </Typography>
 
           <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
@@ -102,21 +102,21 @@ export default function AccountPopover() {
 
         <Divider sx={{ borderStyle: 'dashed' }} />
 
-        <Stack sx={{ p: 1 }}>
-          {OPTIONS.map((option) => (
-            <MenuItem key={option.label} onClick={() => handleClickItem(option.linkTo)}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </Stack>
+        {/* <Stack sx={{ p: 1 }}> */}
+        {/*  {OPTIONS.map((option) => ( */}
+        {/*    <MenuItem key={option.label} onClick={() => handleClickItem(option.linkTo)}> */}
+        {/*      {option.label} */}
+        {/*    </MenuItem> */}
+        {/*  ))} */}
+        {/* </Stack> */}
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        {/* <Divider sx={{ borderStyle: 'dashed' }} /> */}
 
         <MenuItem
           onClick={handleLogout}
           sx={{ m: 1, fontWeight: 'fontWeightBold', color: 'error.main' }}
         >
-          Logout
+          Выйти
         </MenuItem>
       </CustomPopover>
     </>
