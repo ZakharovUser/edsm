ad7596251daa28924371d32cae0de7915104c4a6
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-08-22 12:15:19 +0300

    Add: institute model

---
+++ src/entites/institute/model/index.ts
@@ -0,0 +1,1 @@
+export type { InstituteModel, InstituteListModel } from './institute.model';
---
+++ src/entites/institute/model/institute.model.ts
@@ -0,0 +1,6 @@
+export interface InstituteModel {
+  id: number;
+  name: string;
+}
+
+export type InstituteListModel = InstituteModel[];
