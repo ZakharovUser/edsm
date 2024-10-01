c1d4ea7c0acef4932f9ea1c8aeb57107a1892f16
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-10-01 16:01:55 +0300

    change task status

--- src/features/task/view-task/helpers/task-status-options.ts
+++ src/features/task/view-task/helpers/task-status-options.ts
@@ -1,16 +1,14 @@
-import { TimelineDotProps } from '@mui/lab';
-
 import { TaskStatus } from 'entities/task/model';
 
 export const taskStatusOptions: Record<
   TaskStatus,
   {
     label: string;
-    color: TimelineDotProps['color'];
+    color: 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' | 'default';
   }
 > = {
   [TaskStatus.New]: { color: 'info', label: 'Новая' },
-  [TaskStatus.Draft]: { color: 'grey', label: 'Черновик' },
+  [TaskStatus.Draft]: { color: 'default', label: 'Черновик' },
   [TaskStatus.Canceled]: { color: 'error', label: 'Отменена' },
   [TaskStatus.Progress]: { color: 'secondary', label: 'В работе' },
   [TaskStatus.Completed]: { color: 'success', label: 'Завершена' },
