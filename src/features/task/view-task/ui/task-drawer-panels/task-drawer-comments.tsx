bf14f84542ffb227f0573281f1844fa2c2c78f13
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-10-08 18:27:10 +0300

    Add remark base view

--- src/features/task/view-task/ui/task-drawer-panels/task-drawer-comments.tsx
+++ src/features/task/view-task/ui/task-drawer-panels/task-drawer-comments.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
+import { grey } from 'theme/palette';
 import { useBoolean } from 'hooks/use-boolean';
 
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import MarkUnreadChatAltIcon from '@mui/icons-material/MarkUnreadChatAlt';
 import { DialogTitle, DialogActions, DialogContent, CircularProgress } from '@mui/material';
+
+import { fDateTime } from 'utils/format-time';
+import { formatUserName } from 'utils/format-user-name';
 
 import { Task } from 'entities/task/model';
 import { useCreateTaskComment } from 'entities/task/api';
@@ -30,14 +35,17 @@ export function TaskDrawerComments({ hidden, task, loading }: Props) {
 
   const hasMessages = task?.messages.length !== 0;
 
+  const onClose = () => {
+    dialog.onFalse();
+    setMessage('');
+  };
+
   const onSave = () => {
     if (task) {
       createComment.mutate(
         { taskId: task.task_number, message },
         {
-          onSuccess: () => {
-            setMessage('');
-          },
+          onSuccess: () => onClose(),
         }
       );
     }
@@ -73,23 +81,64 @@ export function TaskDrawerComments({ hidden, task, loading }: Props) {
 
   return (
     <Box hidden={hidden}>
-      {task?.messages.map(({ id, message_text }) => (
-        <Typography key={id}>{message_text}</Typography>
-      ))}
+      <Stack spacing={1} sx={{ width: 1 }}>
+        {task?.messages.map(({ id, message_text, message_date, message_by }) => (
+          <Stack
+            key={id}
+            component={Paper}
+            variant="outlined"
+            sx={{
+              p: 1,
+              width: 1,
+              borderRadius: 1,
+              '&:hover': {
+                bgcolor: 'background.paper',
+                boxShadow: (theme) => theme.customShadows.z4,
+              },
+            }}
+          >
+            <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
+              <Typography variant="subtitle2">{formatUserName(message_by)}</Typography>
+              <Typography variant="caption" color={grey['500']}>
+                #{id}
+              </Typography>
+            </Stack>
+
+            <Typography variant="caption" color={grey['500']}>
+              {fDateTime(message_date)}
+            </Typography>
+
+            <Typography variant="body2" sx={{ mt: 1 }}>
+              {message_text}
+            </Typography>
+          </Stack>
+        ))}
+      </Stack>
 
       <Button onClick={dialog.onTrue}>Add</Button>
 
-      <Dialog open={dialog.value} onClose={dialog.onFalse}>
+      <Dialog open={dialog.value} onClose={onClose}>
         <DialogTitle>Добавить замечание</DialogTitle>
         <DialogContent>
-          <TextField
-            multiline
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-          />
+          <Box
+            sx={{
+              width: 400,
+              pt: 2,
+            }}
+          >
+            <TextField
+              multiline
+              fullWidth
+              label="Замечание"
+              rows={6}
+              size="small"
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
+          </Box>
         </DialogContent>
         <DialogActions>
-          <Button onClick={dialog.onFalse}>Cancel</Button>
+          <Button onClick={onClose}>Cancel</Button>
           <Button onClick={onSave}>Save</Button>
         </DialogActions>
       </Dialog>
--- src/utils/format-user-name.ts
+++ src/utils/format-user-name.ts
@@ -1,13 +1,28 @@
+import { UserModel } from 'entities/user/models';
+
+export function formatUserName(user: UserModel): string;
 export function formatUserName(fullName: string): string;
 export function formatUserName(surname: string, name: string, lastname: string): string;
-export function formatUserName(fullOrSurname: string, name?: string, lastname?: string): string {
+export function formatUserName(
+  fullOrSurnameOrUser: string | UserModel,
+  name?: string,
+  lastname?: string
+): string {
+  if (typeof fullOrSurnameOrUser !== 'string') {
+    const { first_name, middle_name, last_name } = fullOrSurnameOrUser;
+
+    return `${last_name} ${first_name[0].toUpperCase()}.${
+      middle_name ? middle_name[0].toUpperCase().concat('.') : ''
+    }`;
+  }
+
   if (name) {
-    return `${fullOrSurname} ${name[0].toUpperCase()}.${
+    return `${fullOrSurnameOrUser} ${name[0].toUpperCase()}.${
       lastname ? lastname[0].toUpperCase().concat('.') : ''
     }`;
   }
 
-  const [_surname, _name, _lastname] = fullOrSurname.split(' ');
+  const [_surname, _name, _lastname] = fullOrSurnameOrUser.split(' ');
 
   return `${_surname} ${_name[0].toUpperCase()}.${
     _lastname ? _lastname[0].toUpperCase().concat('.') : ''
