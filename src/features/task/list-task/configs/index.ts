fd1687779d699caf304ff3b07fcf1a3c85926211
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-10-12 01:53:37 +0300

    Change open preview on id click

--- src/features/task/list-task/configs/index.ts
+++
@@ -1,1 +0,0 @@
-export { columns } from 'features/task/list-task/configs/columns';
--- src/features/task/list-task/ui/task-data-grid.tsx
+++ src/features/task/list-task/ui/task-data-grid.tsx
@@ -1,23 +1,97 @@
+import { useMemo } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
+import Chip from '@mui/material/Chip';
+import Link from '@mui/material/Link';
 import { DataGridProps } from '@mui/x-data-grid';
 
-import { DataGrid } from 'shared/data-grid/ui';
+import { Cols, TaskImportance } from 'entities/task/model';
 
-import { columns } from '../configs';
+import { DataGrid } from 'shared/data-grid/ui';
 
 // -----------------------------------------------------------------------------------------------------------------
 
 export function TaskDataGrid(props: Omit<DataGridProps, 'columns' | 'onRowClick'>) {
   const [_, setSearchParams] = useSearchParams();
 
-  return (
-    <DataGrid
-      columns={columns}
-      onRowClick={(params) => {
-        setSearchParams(new URLSearchParams({ task: params.id.toString() }));
-      }}
-      {...props}
-    />
+  const columns = useMemo<Cols>(
+    () => [
+      {
+        field: 'id',
+        headerName: '№',
+        flex: 0.3,
+        sortable: false,
+        filterable: false,
+        renderCell: (params) => (
+          <Link
+            onClick={(event) => {
+              event.preventDefault();
+              setSearchParams(new URLSearchParams({ task: params.id.toString() }));
+            }}
+          >
+            {params.id}
+          </Link>
+        ),
+      },
+      {
+        field: 'importance',
+        headerName: 'Важность',
+        flex: 0.5,
+        sortable: true,
+        renderCell: (params) => {
+          const label: TaskImportance = params.value;
+          const color = label === TaskImportance.ordinary ? 'success' : 'error';
+
+          return <Chip label={label} color={color} size="small" variant="outlined" />;
+        },
+      },
+      {
+        field: 'name',
+        headerName: 'Наименование',
+        flex: 1,
+        width: 200,
+        sortable: false,
+      },
+      {
+        field: 'rule',
+        headerName: 'Регламент',
+        flex: 0.5,
+        sortable: false,
+      },
+      {
+        field: 'author',
+        headerName: 'Автор',
+        flex: 0.5,
+        sortable: false,
+        filterable: false,
+      },
+      {
+        field: 'department',
+        headerName: 'Учреждение',
+        flex: 0.5,
+        sortable: false,
+      },
+      {
+        field: 'completion_date',
+        headerName: 'Дата выполнения',
+        flex: 0.5,
+        sortable: false,
+      },
+      {
+        field: 'receipt_date',
+        headerName: 'Дата поступления',
+        flex: 0.5,
+        sortable: false,
+      },
+      {
+        field: 'creation_date',
+        headerName: 'Дата создания',
+        flex: 0.5,
+        sortable: false,
+      },
+    ],
+    [setSearchParams]
   );
+
+  return <DataGrid disableColumnFilter columns={columns} {...props} />;
 }
