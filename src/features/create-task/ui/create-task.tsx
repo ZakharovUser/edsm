bc4516b7eb10eb55d7eb1c0a489d6a96e063668f
Max Zakharov <44815712+ZakharovMaxi@users.noreply.github.com>
2024-11-26 16:21:44 +0300

    Move: create task button

--- src/features/create-task/ui/create-task.tsx
+++ src/features/create-task/ui/create-task.tsx
@@ -1,19 +1,27 @@
 import { useState } from 'react';
+import { useResponsive } from 'hooks/use-responsive';
+
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
 
 import { CreateTaskModal } from './create-task-modal';
-import { CreateTaskButton } from './create-task-button';
 
 // -----------------------------------------------------------------------------------------------------------------
 
 export function CreateTask() {
   const [open, setOpen] = useState(false);
 
+  const downSm = useResponsive('down', 'sm');
+
   const onOpen = () => setOpen(true);
   const onClose = () => setOpen(false);
 
   return (
     <>
-      <CreateTaskButton onClick={onOpen} />
+      <Button color="primary" variant="contained" startIcon={<AddIcon />} onClick={onOpen}>
+        {!downSm ? <Typography>Задача</Typography> : null}
+      </Button>
       <CreateTaskModal open={open} onClose={onClose} />
     </>
   );
